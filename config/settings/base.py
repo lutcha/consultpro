ac0bdd75f882511b694fd1c831b6a7a4eb505d15
lutch <lutchafrederico@gmail.com>
2026-05-05 22:42:04 -0100

    fix: disable whitenoise strict manifest to prevent collectstatic failure on missing maps

--- config/settings/base.py
+++ config/settings/base.py
@@ -220,6 +220,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_MANIFEST_STRICT = False
 
 # Media files
 MEDIA_URL = '/media/'
